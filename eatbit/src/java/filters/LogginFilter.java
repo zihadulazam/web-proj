611595fc3d9f41303a8e46501e136d34bbe0c0f7
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-05-21 17:07:43 +0200

    Add files via upload

---
+++ eatbit/src/java/filters/LogginFilter.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author User
+ */
+public class LogginFilter implements Filter {
+    
+  
+    public void init(FilterConfig fc) throws ServletException {
+        
+    }
+
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        final HttpServletRequest req = (HttpServletRequest)sr;
+        final HttpServletResponse resp = (HttpServletResponse)sr1;
+
+        // controllo se la sessione esiste e contiene un utente loggato
+        // se non esiste o non conteiene un utente, ridirigo alla pagina iniziale di login
+        HttpSession session = (req).getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath());
+        } else {
+            fc.doFilter(sr, sr1);
+        }
+    }
+
+    public void destroy() {
+        
+    }
+}
---
+++ eatbit/src/java/servlets/LoginServlet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import database.DbManager;
+import database.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    private DbManager manager;
+    
+    @Override
+    public void init() throws ServletException {
+        // inizializza il DBManager dagli attributi di Application
+        this.manager = (DbManager)super.getServletContext().getAttribute("dbmanager");
+    }
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        // controllo nel DB se esiste un utente con lo stesso username + password
+        User user;
+        
+            user =null; //manager.authenticate(username, password);
+
+        // se non esiste, ridirigo verso pagina di login con messaggio di errore
+        if (user == null) {
+            // metto il messaggio di errore come attributo di Request, così nel JSP si vede il messaggio
+            request.setAttribute("message", "Username/password non esistente !");
+            RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+            rd.forward(request, response);
+
+        } else {
+
+            // imposto l'utente connesso come attributo di sessione
+            // per adesso e' solo un oggetto String con il nome dell'utente, ma posso metterci anche un oggetto User
+            // con, ad esempio, il timestamp di login
+            HttpSession session = request.getSession(true);
+            session.setAttribute("user", user);
+
+            // mando un redirect alla servlet che carica i prodotti
+            response.sendRedirect(request.getContextPath() + "/userProfile.jsp");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
