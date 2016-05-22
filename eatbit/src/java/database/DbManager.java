201e48e56c00cb84d26fae59eb0b5a3bab555791
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2016-05-22 17:30:02 +0200

    aggiunti metodi per recuperare: foto segnalate, review segnalate, replies da confermare, richieste di creazione/claim/both, notifiche dell'utente, aggiunto un po di javadoc, ricordo che fino adesso on è stato testato niente a livello di db quindi se trovate errori scrivete pure nel canale database

--- eatbit/src/java/database/DbManager.java
+++ eatbit/src/java/database/DbManager.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Vector;
 import java.util.logging.Level;
@@ -33,7 +34,7 @@ public class DbManager {
     
       /**
      * Prova ad inserire(registrare sul sito) un utente nel database.
-     * @param user Il bean user con i dati all'iterno
+     * @param user Oggetto User con i dati all'iterno
      * @return 0 se è andata a buon fine, 1 se nn ha registrato xk esiste un
      * utente con quella email, 2 se esiste un utente con quel nick, 3 se non
      * è andata a buon fine per altri motivi
@@ -86,7 +87,7 @@ public class DbManager {
         return res;
     }
     /**
-     * 
+     * Per autenticare uno User, di cui si fornisce nick/mail e password.
      * @param nickOrEmail nick o email dell utente che fa login
      * @param password password
      * @return null se la password non matcha o se l'utente non esiste, uno User con password "pulita via" altrimenti
@@ -164,14 +165,14 @@ public class DbManager {
         return res;
     }
     /**
-     * 
+     * Restituisce le review di un utente.
      * @param user L'utente di cui cercare le reviews.
-     * @return Un vettore di oggetti Review dell'utente.
+     * @return Un ArrayList di oggetti Review dell'utente.
      * @throws java.sql.SQLException
      */
-    public Vector<Review> getUserReviews(User user) throws SQLException
+    public ArrayList<Review> getUserReviews(User user) throws SQLException
     {
-        Vector<Review> reviews=new Vector();
+        ArrayList<Review> reviews=new ArrayList();
         try
         (PreparedStatement st = con.prepareStatement("select * from REVIEWS where ID_CREATOR=?")) 
         {
@@ -198,20 +199,19 @@ public class DbManager {
                     reviews.add(review);
                 }
             }
-            
         }
         return reviews;
     }
     
     /**
-     * 
+     * Le notificazioni di un utente (n.b. NON per admin).
      * @param user L'utente di cui si vuole cercare le notifiche.
-     * @return Un vettore contenente oggetti di tipo Notification.
+     * @return Un ArrayList contenente oggetti di tipo Notification.
      * @throws SQLException 
      */
-    public Vector<Notification> getUserNotifications(User user) throws SQLException
+    public ArrayList<Notification> getUserNotifications(User user) throws SQLException
     {
-        Vector<Notification> notifications=new Vector();
+        ArrayList<Notification> notifications=new ArrayList();
         try
         (PreparedStatement st = con.prepareStatement("select * from NOTIFICATIONS where ID=?")) 
         {
@@ -235,6 +235,332 @@ public class DbManager {
         return notifications;
     }
     
+    /**
+     * Restituisce un numero di contesti, una classe formata da una review, uno user e una reply;
+     * lo user è chi ha fatto la reply, la review è ciò a cui la reply vuole rispondere.
+     * Da usare p.e. per mostrare ad un admin per confermare o no una reply.
+     * Il timestamp di un contesto "prelevato" viene refrehsato al timestamp attuale
+     * in modo che non sarà prelevabile da altri admin per 1 ora.
+     * @param many Quanti contesti (al max) da ricevere.
+     * @return Un ArrayList di contesti.
+     * @throws SQLException 
+     */
+    public ArrayList<ReplyContext> getRepliesToBeConfirmed(int many) throws SQLException
+    {
+        ArrayList<ReplyContext> contesti= new ArrayList();
+        try
+        (PreparedStatement st= con.prepareStatement("select * from REPLIES_TO_BE_CONFIRMED"
+                + " where {fn TIMESTAMPDIFF( SQL_TSI_HOUR, DATE_ADMIN_TOOK,?)} >= 1 "
+                + "FETCH FIRST ? ROWS ONLY"))
+        {
+            Timestamp timestamp= new Timestamp(Calendar.getInstance().getTime().getTime());
+            st.setTimestamp(1,timestamp);
+            st.setInt(2, many);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                while(rs.next())
+                {
+                    contesti.add(getReplyContext(rs.getInt("ID")));
+                    rs.updateTimestamp("DATE_ADMIN_TOOK", timestamp);
+                    con.commit();
+                }
+            }
+        }
+        return contesti;
+    }
+    
+    private ReplyContext getReplyContext(int id_reply) throws SQLException
+    {
+        ReplyContext contesto= new ReplyContext();
+        contesto.setReply(getReplyFromId(id_reply));
+        contesto.setReview(getReviewById(contesto.getReply().getId_review()));
+        contesto.setUser(getUserById(contesto.getReply().getId_owner()));
+        return contesto;
+    }
+    
+    private Reply getReplyFromId(int id_reply) throws SQLException
+    {
+        Reply reply= null;
+        try(PreparedStatement st= con.prepareStatement("select * from REPLIES where id=?"))
+        {
+            st.setInt(1, id_reply);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                if(rs.next())
+                {
+                    reply= new Reply();
+                    reply.setId(id_reply);
+                    reply.setDescription(rs.getString("DESCRIPTION"));
+                    reply.setDate_creation(rs.getTimestamp("DATE_CREATION"));
+                    reply.setId_review(rs.getInt("ID_REVIEW"));
+                    reply.setId_owner(rs.getInt("ID_OWNER"));
+                    reply.setDate_validation(rs.getTimestamp("DATE_VALIDATION"));
+                    reply.setId_validator(rs.getInt("ID_VALIDATOR"));
+                    reply.setValidated(rs.getBoolean("VALIDATED"));
+                }
+            }
+        }
+        return reply;
+    }
+    
+    private Review getReviewById(int id) throws SQLException
+    {
+        Review review=null;
+        try(PreparedStatement st= con.prepareStatement("select * from REVIEWS where id=?"))
+        {
+            st.setInt(1,id);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                if(rs.next())
+                {
+                    review= new Review();
+                    review.setId(rs.getInt("ID"));
+                    review.setGlobal_value(rs.getInt("GLOBAL_VALUE"));
+                    review.setFood(rs.getInt("FOOD"));
+                    review.setService(rs.getInt("SERVICE"));
+                    review.setValue_for_money(rs.getInt("VALUE_FOR_MONEY"));
+                    review.setAtmosphere(rs.getInt("ATMOSPHERE"));
+                    review.setName(rs.getString("NAME"));
+                    review.setDescription(rs.getString("DESCRIPTION"));
+                    review.setDate_creation(rs.getTimestamp("DATE_CREATION"));
+                    review.setId_restaurant(rs.getInt("ID_RESTAURANT"));
+                    review.setId_creator(rs.getInt("ID_CREATOR"));
+                    review.setId_photo(rs.getInt("ID_PHOTO"));
+                    review.setLikes(rs.getInt("LIKES"));
+                }
+            }
+        }
+        return review;
+    }
+    /**
+     * 
+     * @param id
+     * @return Uno user con la password rimossa, null se non esiste o qualcosa va storto.
+     * @throws SQLException 
+     */
+    private User getUserById(int id) throws SQLException
+    {
+        User user=null;
+        try(PreparedStatement st= con.prepareStatement("select * from USERS where id=?"))
+        {
+            st.setInt(1, id);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                if(rs.next())
+                {
+                    user=new User();
+                    user.setId(rs.getInt("ID"));
+                    user.setName(rs.getString("NAME"));
+                    user.setSurname(rs.getString("SURNAME"));
+                    user.setNickname(rs.getString("NICKNAME"));
+                    user.setEmail(rs.getString("EMAIL"));
+                    user.setPassword("placeholder");                
+                    user.setAvatar_path(rs.getString("AVATAR_PATH"));
+                    user.setReviews_counter(rs.getInt("REIVEWS_COUNTER"));
+                    user.setReviews_positive(rs.getInt("REVIEWS_POSITIVE"));
+                }
+            }
+        }
+        return user;
+    }
+    /**
+     * Funzione per prendere una serie di photo segnalate e i loro contesti, 
+     * solitamente usato per l'admin. Un contesto è la foto più lo user che lha 
+     * postata. Una volta che un contesto è prelevato (restituito) non sarà
+     * possibile restituirlo ancora tramite questo metodo per un ora.
+     * @param many Quanti contesti (al max) si vuole ricevere.
+     * @return Un ArrayList di contesti.
+     * @throws SQLException 
+     */
+    public ArrayList<PhotoContext> getReportedPhotos(int many) throws SQLException
+    {
+        ArrayList<PhotoContext> contesti= new ArrayList();
+        try
+        (PreparedStatement st= con.prepareStatement("select * from REPORTED_PHOTOS"
+                + " where {fn TIMESTAMPDIFF( SQL_TSI_HOUR, DATE_ADMIN_TOOK,?)} >= 1 "
+                + "FETCH FIRST ? ROWS ONLY"))
+        {
+            Timestamp timestamp= new Timestamp(Calendar.getInstance().getTime().getTime());
+            st.setTimestamp(1,timestamp);
+            st.setInt(2, many);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                while(rs.next())
+                {
+                    contesti.add(getPhotoContext(rs.getInt("ID")));
+                    rs.updateTimestamp("DATE_ADMIN_TOOK", timestamp);
+                    con.commit();
+                }
+            }
+        }
+        return contesti;
+    }
+    
+    private PhotoContext getPhotoContext(int id_photo) throws SQLException
+    {
+        PhotoContext contesto= new PhotoContext();
+        contesto.setPhoto(getPhotoById(id_photo));
+        contesto.setUser(getUserById(contesto.getPhoto().getId_owner()));
+        return contesto;
+    }
+    
+    private Photo getPhotoById(int id) throws SQLException
+    {
+        Photo photo= null;
+        try(PreparedStatement st= con.prepareStatement("select * from PHOTOS where id=?"))
+        {
+            st.setInt(1, id);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                if(rs.next())
+                {
+                    photo= new Photo();
+                    photo.setId(id);
+                    photo.setName(rs.getString("NAME"));
+                    photo.setDescription(rs.getString("DESCRIPTION"));
+                    photo.setPath(rs.getString("PATH"));
+                    photo.setId_restaurant(rs.getInt("ID_RESTAURANT"));
+                    photo.setId_owner(rs.getInt("ID_OWNER"));
+                }
+            }
+        }
+        return photo;
+    }
+    
+    private Restaurant getRestaurantById(int id) throws SQLException
+    {
+        Restaurant restaurant= null;
+        try(PreparedStatement st= con.prepareStatement("select * from RESTAURANTS where id=?"))
+        {
+            st.setInt(1, id);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                if(rs.next())
+                {
+                    restaurant= new Restaurant();
+                    restaurant.setId(rs.getInt("ID"));
+                    restaurant.setName(rs.getString("NAME"));
+                    restaurant.setDescription(rs.getString("DESCRIPTION"));
+                    restaurant.setWeb_site_url(rs.getString("WEB_SITE_URL"));
+                    restaurant.setGlobal_value(rs.getInt("GLOBAL_VALUE"));
+                    restaurant.setId_owner(rs.getInt("ID_OWNER"));
+                    restaurant.setId_creator(rs.getInt("ID_CREATOR"));
+                    restaurant.setId_price_range(rs.getInt("ID_PRICE_RANGE"));
+                    restaurant.setReviews_counter(rs.getInt("REVIEWS_COUNTER"));
+                    restaurant.setValidated(rs.getBoolean("VALIDATED"));
+                }
+            }
+        }
+        return restaurant;
+    }
+    
+    private AttemptContext getAttemptContext(int id_user, int id_restaurant, int creation_claim_both) throws SQLException
+    {
+        AttemptContext contesto= new AttemptContext();
+        contesto.setUser(getUserById(id_user));
+        contesto.setRestaurant(getRestaurantById(id_restaurant));
+        contesto.setIsClaim(creation_claim_both);
+        return contesto;
+    }
+    
+    /**
+     * Funzione per prendere una serie di richieste di ristoranti e i loro contesti, 
+     * solitamente usato per l'admin. Un contesto è l'utente che ha fatto la richiesta,
+     * il ristorante riguarda il ristorante in questione e una flag che indica il
+     * tipo di richiesta.
+     * Una volta che un contesto è restituito da questa funzione non sarà
+     * possibile restituirlo ancora tramite questo metodo per un ora.
+     * @param many Quanti contesti (al max) si vuole ricevere.
+     * @return Un ArrayList di contesti.
+     * @throws SQLException 
+     */
+    public ArrayList<AttemptContext> getRestaurantsRequests(int many) throws SQLException
+    {
+        ArrayList<AttemptContext> contesti= new ArrayList();
+        try
+        (PreparedStatement st= con.prepareStatement("select * from RESTAURANTS_REQUESTS"
+                + " where {fn TIMESTAMPDIFF( SQL_TSI_HOUR, DATE_ADMIN_TOOK,?)} >= 1 "
+                + "FETCH FIRST ? ROWS ONLY"))
+        {
+            Timestamp timestamp= new Timestamp(Calendar.getInstance().getTime().getTime());
+            st.setTimestamp(1,timestamp);
+            st.setInt(2, many);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                while(rs.next())
+                {
+                    contesti.add(getAttemptContext(rs.getInt("ID"),rs.getInt("ID_RESTAURANT"),
+                            rs.getInt("CREATION_CLAIM_BOTH_FLAG")));
+                    rs.updateTimestamp("DATE_ADMIN_TOOK", timestamp);
+                    con.commit();
+                }
+            }
+        }
+        return contesti;
+    }
+    
+    /**
+     *Per ricevere un numero pari a many di contesti (user+review) riguardanti
+     * le review che hanno subito un report. 
+     * Una volta che un contesto è stato restituito da questo metodo non sarà
+     * restituit per un ora.
+     * @param many Quanti contesti (al max) si vuole ricevere.
+     * @return Un ArrayList con i contesti.
+     * @throws SQLException
+     */
+    public ArrayList<ReviewContext> getReportedReviews(int many) throws SQLException
+    {
+        ArrayList<ReviewContext> contesti= new ArrayList();
+        try
+        (PreparedStatement st= con.prepareStatement("select * from REPORTED_REVIEWS"
+                + " where {fn TIMESTAMPDIFF( SQL_TSI_HOUR, DATE_ADMIN_TOOK,?)} >= 1 "
+                + "FETCH FIRST ? ROWS ONLY"))
+        {
+            Timestamp timestamp= new Timestamp(Calendar.getInstance().getTime().getTime());
+            st.setTimestamp(1,timestamp);
+            st.setInt(2, many);
+            try
+            (ResultSet rs= st.executeQuery())
+            {
+                con.commit();
+                while(rs.next())
+                {
+                    contesti.add(getReviewContext(rs.getInt("ID")));
+                    rs.updateTimestamp("DATE_ADMIN_TOOK", timestamp);
+                    con.commit();
+                }
+            }
+        }
+        return contesti;
+    }
+    
+    private ReviewContext getReviewContext(int review_id) throws SQLException
+    {
+        ReviewContext contesto= new ReviewContext();
+        contesto.setReview(getReviewById(review_id));
+        contesto.setUser(getUserById(contesto.getReview().getId_creator()));
+        return contesto;
+    }
+    
+    /**
+     * Chiude la connessione al database!
+     */
     public static void shutdown() 
     {
         try {
